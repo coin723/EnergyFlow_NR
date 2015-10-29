15705669a7b85173756493ff5d4d6e768eae3b27
Yongjun <coin723@gmail.com>
2015-10-30 02:15:11 +0900

    Repository generated

---
+++ gatherData.R
@@ -0,0 +1,4 @@
+pro_2013 <- read.csv('pro-2013-xls.csv', encoding = 'utf-8', stringsAsFactors = FALSE)
+sup_2013 <- read.csv('sup-2013-xls.csv', encoding = 'utf-8', stringsAsFactors = FALSE)
+use_2013_field <- read.csv('use-2013-field-xls.csv', encoding = 'utf-8', stringsAsFactors = FALSE)
+use_2013_source <- read.csv('use-2013-source-xls.csv', encoding = 'utf-8', stringsAsFactors = FALSE)
